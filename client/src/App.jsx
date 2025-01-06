430985a450361184f4965f4bca24eb644b97706e
Mohammad Obaid <obaidulla2249@gmail.com>
2025-01-06 21:45:35 +0530

    frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,5 @@
+import {Button} from "./components/ui/button"
+const App=()=>{
+   return <div className="w-screen h-screen bg-sky-200 flex items-center justify-center"> <Button variant="ghost">Click Me</Button></div>
+}
+export default App;
